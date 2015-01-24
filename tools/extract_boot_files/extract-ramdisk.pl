bf03464b014aa89baa09e161c718000ea8c7d733
solitarily <chuangweida@gmail.com>
2015-01-24 18:42:26 +0800

    云卓论坛-越狱 汉化

--- tools/extract_boot_files/extract-ramdisk.pl
+++ tools/extract_boot_files/extract-ramdisk.pl
@@ -28,25 +28,13 @@ my($kernelSizeInPages) = int(($kernelSize + $pageSize - 1) / $pageSize);
 my($ram1Addr) = (1 + $kernelSizeInPages) * $pageSize;
 
 my($ram1) = substr($bootimg, $ram1Addr, $ram1Size);
-
-my $compressformat = "gz";
-my $compressprogram = "gzip -d -c";
-
-if (substr($ram1, 0, 2) eq "\x1F\x8B")
+$ram1 = substr($ram1, 512);
+if (substr($ram1, 0, 2) ne "\x1F\x8B")
 {
-       $compressformat = "gz";
-       $compressprogram = "gzip -d -c";
-}
-elsif (substr($ram1, 0, 2) eq "\x02\x21")
-{
-       $compressformat = "lz4";
-       $compressprogram = "lz4c -d";
-}
-else{
-	die "The boot image does not appear to contain a valid gzip or lz4 file";
+	die "The boot image does not appear to contain a valid gzip file";
 }
 
-open (RAM1FILE, ">$ARGV[0]-ramdisk.cpio.$compressformat");
+open (RAM1FILE, ">$ARGV[0]-ramdisk.cpio.gz");
 binmode(RAM1FILE);
 print RAM1FILE $ram1 or die;
 close RAM1FILE;
@@ -58,5 +46,5 @@ if (-e "$ARGV[0]-ramdisk") {
 
 mkdir "$ARGV[0]-ramdisk" or die;
 chdir "$ARGV[0]-ramdisk" or die;
-system ("$compressprogram ../$ARGV[0]-ramdisk.cpio.$compressformat | cpio -i");
-system ("rm -f ../$ARGV[0]-ramdisk.cpio.$compressformat");
+system ("gzip -d -c ../$ARGV[0]-ramdisk.cpio.gz | cpio -i");
+system ("rm -f ../$ARGV[0]-ramdisk.cpio.gz");
